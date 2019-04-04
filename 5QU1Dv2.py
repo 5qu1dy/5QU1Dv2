63146c810053cabd496a5c1a391cfd98c3607ff8
Oktavianuzzz <48722600+Oktavianuzzz@users.noreply.github.com>
2019-04-04 20:13:44 +0700

    Add files via upload

---
+++ 5QU1Dv2.py
@@ -0,0 +1,100 @@
+#Tools Ini Ditulis Oleh Mr.5QU1D...!!
+#DONT REMAKE..!
+#CAPEK CUK BUAT TOOLS KEK GINI! MAKANYA JAN DIBUAT ULANG, OKEH!?
+
+import os
+import time
+os.system ("pkg install python;pkg install git;pkg install toilet;pkg install ruby;gem install lolcat;pkg install figlet")
+
+print ("                                                                                                    ")
+print ("                                                                                                    ")
+print ("                                       `   ``    `                                                  ")
+print ("                                  `-sdNNNNNNNmNNNNNdsss+-                                           ")
+print ("                              `+ydNNMMMMMMMMMMMMMMMMMMMMNmy+/-`                                     ")
+print ("                           .-ymMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNy`                                   ")
+print ("                         .oNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm/                                  ")
+print ("                        omMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo                                 ")
+print ("                      `+mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy`                               ")
+print ("                    .ohNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd                               ")
+print ("                 `+mMMMMMMMMMMMMMMMMmdhs+/--:odMMMMMMMMMMMMMMMMMMMMMs`                              ")
+print ("                  sNMMMMMMMMMMMMMNdo-``        `:+yMMMMMMMMMMMMMMMMMMM+                             ")
+print ("                  yMMMMMMMNmmdho/-`                MMMMMMMMMMMMMMMMMMMN                             ")
+print ("                  oMMMMMmo-.``                     MMMMMMMMMMMMMMMMMMMM-                            ")
+print ("                  .+NMMd.                          hNMMMMMMMMMMMMMMMMMMy                            ")
+print ("                   `/mM-   ``  `::--                -yNMMMMMMMMMMMMNNNNd                            ")
+print ("                    :Nd `-:------:/+//.               :dMMMMMMMMMNo:///s.                           ")
+print ("                    :Mo`/.-/o : oNMMMNNhssss/.`        `yMMMMMMMm/:-```/s`                          ")
+print ("                    .m/`/dMMo`+`/+ooydNNNNNNMNd:.`      `NMMMMNo`.`    `o/                          ")
+print ("                     .shMMN+`+:.      -::---/sy:/.       hMMMh.   :-  ...+                          ")
+print ("                     oMMMd.` ``      `:/oo::.::+:-       oddh`   .:`` -y./                          ")
+print ("                    /MMMh:---:::  +  -hho+//-//-.                /`   :s:-                          ")
+print ("                    .Nm- .+sssd+` .   :..-:-.                    `+  `+/:-                          ")
+print ("                     :/.yms:--s/.      ``                         o:.   /`                          ")
+print ("                      -so/---/+o                                  .    /.                           ")
+print ("                       s+.  -:`/         -.                          -s.                            ")
+print ("                       +   :- +      `:-+ .::`                    +-:Nm                             ")
+print ("                      /.  .: +-     .-..+    -:`    -             + +Mo                             ")
+print ("                      +   + `+:.  /-` ``       :-`` --            o dM.                             ")
+print ("                      :-  .:`.:.      ..-----.  /.+  +           `d NN                              ")
+print ("                       :. -:    .---:/+shhdddNds.++  +            o`Mh                              ")
+print ("                        :.`+  /s++soh+y-+.o/+dym+++ `/            +`d.                              ")
+print ("                         :--:`+mMh/.:-ooNmMNMMNm.`+ +`            +`+                               ")
+print ("                          -:`::ssmdmNNNNdyhsso//  +.:             + +                               ")
+print ("                           `:.`///odds+o/:-.---  `++             `+ +                               ")
+print ("                             .:./::-----.---``   /::            -:` +                               ")
+print ("                              `:-+```````      `:.+           `:.   /:                              ")
+print ("                                :/:            . ..          `/`     /:                             ")
+print ("                                 +`             `            :        +                             ")
+print ("                                 +             --                     .+:`                          ")
+print ("                                 -:`        `.:.                       o:/.                         ")
+print ("                                  `:+-..`---..                         .h`-:`                       ")
+print ("                                  `/:/.//.                              /s `.--`                    ")
+print ("                                 `/` :. `:`                              o    `-/                   ")
+print ("                                `/    /`                                 +      `-                  ")
+print ("                                :      -`                               `/                          ")
+print ("                                                                        /`             V2.0         ")
+print ("                                                                                                    ")
+print ("                                                                                                    ")
+print ("                                                                                                    ")
+
+time.sleep(1)
+
+os.system ('toilet -f standard "5QU1Dv2" -F gay')
+time.sleep(1)
+print ("		% Author :	Mr.5QU1DY %")
+print ("		% Mastah :	Mr.Tralala %")
+print ("		% TEAM :	FAMILY CYBER INDO F.C.I %")
+print ("		% Github :	https://github.com/Oktavianuzzz %")
+print ("		% Tools Version : V2.0 %")
+print (" ")
+print ("Selection Menu : ")
+menu='''
+1 • METASPLOIT
+2 • DEFACE
+3 • A-RAT
+
+0 • Close
+'''
+print (menu)
+selection=input ('Enter Your Selection >> ')
+if selection=='1':
+   print ('Installing Metasploit...!')
+   time.sleep(1)
+   print ('Loading.............')
+   os.system ("pkg update && pkg upgrade -y;pkg install unstable-repo;pkg install metasploit;msfconsole")
+elif selection=='2':
+     print ("You can Deface the Website with This Method...!")
+     time.sleep(1)
+     print ("curl -T scriptdeface.html www.target.com")
+     time.sleep(1)
+     print ("Thanks!")
+elif selection=='3':
+     print ("Installing A-Rat...!")
+     print ("Loading...........")
+     os.system ("pkg update && pkg upgrade -y;apt install git;apt install python2 python;git clone https://github.com/Xi4u7/A-Rat;cd A-Rat;python2 A-Rat.py")
+elif selection=='0':
+     print ("Closing.....")
+     os.system ("exit")
+
+else:
+    print ("ERROR: WRONG INPUT...!!!")
